06b5c132c6a237a73067467b0ab41112fc36a628
vvvin333 <333vvvin@gmail.com>
2021-01-21 14:12:28 +0200

    full node.js site with html/js processing

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const app = require("express")();
 
-const PORT = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
-	res.send("Hello!");
+const PORT = process.env.PORT || 8000;
+
+/*
+app.use(express.static(__dirname + '/public'));
+//for images/text etc files
+*/
+
+
+app.get('/', function(request, response){
+    response.sendFile(__dirname +'/index.html');
 });
 
+app.get('*.(html|js)', function(request, response){
+    response.sendFile(__dirname +request['url']);
+    //console.log(request['url']);
+});
+
+/*
+app.get("", (req, res) => {
+	res.send("Hello!");
+});*/
+
 app.listen(PORT, () => {
-	console.log('App up at port ${PORT}')
+	console.log("App up at port " +PORT)
 });
